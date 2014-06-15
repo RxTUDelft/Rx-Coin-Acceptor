b9d337158cbc5e3878db5258b0c96709b068886a
David Hoepelman <dhoepelman@gmail.com>
2014-06-15 19:45:09 +0200

    Base class for CoinAcceptors

---
+++ src/main/java/nl/tudelft/rx/AbstractCoinAcceptor.java
@@ -0,0 +1,60 @@
+package nl.tudelft.rx;
+
+import rx.Observable;
+import rx.Subscriber;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * An abstract coin acceptor which contains base methods for all coin acceptors
+ */
+public abstract class AbstractCoinAcceptor implements CoinAcceptor {
+    /**
+     * A list of subscribers to coin events
+     */
+    private final List<Subscriber<? super Coin>> subscribers = Collections.synchronizedList(new ArrayList<>());
+
+    /**
+     * Give completed event to all subscribers and forget them.
+     * Every subclass that overrides this should call super.stop()
+     */
+    public void stop() {
+        synchronized (subscribers) {
+            // Signal completed to all current subscribers
+            for (Subscriber sub : subscribers) {
+                sub.onCompleted();
+            }
+            // Remove all current subscribers
+            subscribers.clear();
+        }
+    }
+
+    /**
+     * Subclasses should call this when a new coin is detected
+     */
+    protected final void newCoin(Coin coin) {
+        synchronized (subscribers) {
+            for (Subscriber sub : subscribers) {
+                sub.onNext(coin);
+            }
+        }
+    }
+
+    protected final void error(Throwable t) {
+        synchronized (subscribers) {
+            for (Subscriber sub : subscribers) {
+                sub.onError(t);
+            }
+            subscribers.clear();
+        }
+    }
+
+    @Override
+    public final Observable<Coin> coins() {
+        return Observable.create((Subscriber<? super Coin> subscriber) -> {
+            subscribers.add(subscriber);
+        });
+    }
+}
--- src/main/java/nl/tudelft/rx/CoinAcceptor.java
+++ src/main/java/nl/tudelft/rx/CoinAcceptor.java
@@ -6,5 +6,14 @@ import rx.Observable;
  * An interface for a coin acceptor
  */
 public interface CoinAcceptor {
+    /**
+     * Get the Observable of coins, a "hot" Observable which will give a result whenever a coin is inserted
+     */
     public Observable<Coin> coins();
+
+    /**
+     * Makes the CoinAcceptor stop listening for coins.
+     * Will cause all current Observers to receive a completed event.
+     */
+    public void stop();
 }
